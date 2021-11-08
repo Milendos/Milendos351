1c0e9e0e5afdd579621b0ff831ffca96e3b1dee1
nvrl0vd <@CN404-03>
2021-11-08 11:58:31 +0300

    Файлы проекта

---
+++ Massive28090/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive28090
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Массив
+            backData:
+            Console.WriteLine("Введите границы массива:");
+            int BorderArray = int.Parse(Console.ReadLine());
+
+            string[] ArrayText = new string[BorderArray]; //5
+            for (int step = 0; step < ArrayText.Length; step++)
+            {
+                Console.Write($"Значение для {step}-индекса: ");
+                Console.WriteLine();
+                ArrayText[step] = Console.ReadLine();
+            }
+            Console.WriteLine("Ввод данных закончен! Ура! :3");
+
+            Console.WriteLine("Введеный ранее вами массив:");
+            foreach (string item in ArrayText)
+            {
+                Console.WriteLine(item);
+            }
+            goto backData;
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choose = 3;
+            string name = null;
+            int pro = 0;
+            Console.WriteLine("Добро пожаловать на Олимпийские игры!");
+
+            Console.WriteLine("Чтобы вывести данные, нажмите 1");
+            Console.WriteLine("Чтобы зарегистрировать спортсменов, нажмите 2");
+            Console.WriteLine("Чтобы начать стрельбу, нажмите 3");
+
+            Console.Write("Выберите ваше действие- ");
+
+            choose = int.Parse(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+
+                    Console.WriteLine("Расстояние дистанции 18, 30 или 50 метров.");
+                    Console.WriteLine("Виды луков - классический и блочный.");
+
+                    break;
+                case 2:
+                    Console.WriteLine("Регистрация спортсменов");
+                    Console.Write("Какое количество спортсменов будет выступать? -");
+
+                    name = null.Parse(Console.ReadLine());
+
+
+
+
+
+                    break;
+                    
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
